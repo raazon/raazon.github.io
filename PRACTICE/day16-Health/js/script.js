7dce171ad230dd095528efa521b7b1eb56f226be
raazon <raazzon@gmail.com>
2017-01-24 14:54:10 +0600

    Final Health Html5 Template

--- PRACTICE/day16-Health/js/script.js
+++ PRACTICE/day16-Health/js/script.js
@@ -8,12 +8,18 @@ jQuery('#widget-accordion').collapse()
 
 // prettyPhoto
 
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
     jQuery("a[rel^='prettyPhoto']").prettyPhoto({
         deeplinking: false,
-        animation_speed:'normal',
-        theme:'light_square',
-        slideshow:5000,
+        animation_speed: 'normal',
+        theme: 'light_square',
+        slideshow: 5000,
         autoplay_slideshow: true,
     });
+});
+
+// Off canvas return false
+
+jQuery('a.menu-btn').click(function(){
+    return false;
 });
