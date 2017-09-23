65aae590c674307eee518f2f4e87120fa6382ea1
Themepeck <themepeckpro@gmail.com>
2017-09-23 22:56:27 +0600

    portfolios updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,6 +114,25 @@
 					$('#headerToggle, #header, #main')
 						.css('transition', 'none');
 
+		// Image load http://dinbror.dk/blazy/
+	    var bLazy = new Blazy({
+	        breakpoints: [{
+		    width: 420 // Max-width
+	          , src: 'data-src-small'
+		}]
+	      , success: function(element){
+		    setTimeout(function(){
+			// We want to remove the loader gif now.
+			// First we find the parent container
+			// then we remove the "loading" class which holds the loader image
+			var parent = element.parentNode;
+			parent.className = parent.className.replace(/\bloading\b/,'');
+		    }, 200);
+	        }
+	   });
+
+
+
 	});
 
 })(jQuery);
