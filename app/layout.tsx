caa09ba1b8be3b7f210eba8008fb8b6a3cfed3e4
Razon Komar Pal <raazon@users.noreply.github.com>
2024-12-30 05:57:42 +0600

    Fix assets loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import Script from 'next/script';
-import './assets/css/theme-1.css';
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import './theme-1.css';
 
 const roboto = Roboto({
 	weight: ['100', '300', '400', '500', '700'],
