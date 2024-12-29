ed18f32be55fde495b8d3fbdd5cd419e780809f2
Razon Komar Pal <raazon@users.noreply.github.com>
2024-12-30 05:00:07 +0600

    delete old files

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import CustomNavLink from "./CustomNavLink/CustomNavLink";
-import Icon from "./Icon/Icon";
-import Link from "./Link/Link";
-
-export { CustomNavLink, Icon, Link };
-
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Home from "./Home/Home";
-import NotFound from "./NotFound/NotFound";
-import Resume from "./Resume/Resume";
-
-export { Home, NotFound, Resume };
-
