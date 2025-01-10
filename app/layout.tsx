0fd1e354a74ddedf7e62ae92b4756dac01eed918
Razon Komar Pal <raazon@users.noreply.github.com>
2025-01-11 01:18:00 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 	return (
 		<html lang="en" className={roboto.className}>
 			<head>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<link rel="icon" href="/favicon.ico" />
 				<link rel="apple-touch-icon" href="/logo192.png" />
 			</head>
