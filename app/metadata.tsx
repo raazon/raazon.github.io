28ca92f76b696c9495eb43295403ea9885269448
Razon Komar Pal <raazon@users.noreply.github.com>
2025-01-11 00:59:40 +0600

    Update metadata.tsx

--- app/metadata.tsx
+++ app/metadata.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Razon Komar Pal | Expert Full Stack Web Developer",
-	description: "Razon Komar Pal is an expert Full Stack Web Developer specializing in modern web technologies and responsive design.",
-	keywords: "Full Stack Web Developer, Web Development, Responsive Design, Modern Web Technologies, WordPress Developer, Razon Komar Pal",
+	title: "Razon Komar Pal | Expert Web Developer",
+	description: "Razon Komar Pal is an expert Web Developer specializing in modern web technologies and responsive design.",
+	keywords: "Web Developer, Web Development, Responsive Design, Modern Web Technologies, WordPress Developer, Razon Komar Pal",
 	openGraph: {
-		title: "Razon Komar Pal | Expert Full Stack Web Developer",
-		description: "Razon Komar Pal is an expert Full Stack Web Developer specializing in modern web technologies and responsive design.",
+		title: "Razon Komar Pal | Expert Web Developer",
+		description: "Razon Komar Pal is an expert Web Developer specializing in modern web technologies and responsive design.",
 		siteName: "Razon Komar Pal Portfolio",
 		locale: "en_US",
 		type: "website",
