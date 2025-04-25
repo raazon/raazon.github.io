b944d9c835013b43671110368ae0059e43b53691
Razon Komar Pal <raazon@users.noreply.github.com>
2025-04-25 23:41:31 +0600

    Update index.tsx

--- page-components/homepage/index.tsx
+++ page-components/homepage/index.tsx
@@ -1,4 +1,5 @@
 
 export { default as AboutMe } from './AboutMe';
 export { default as Overview } from './Overview';
+export { default as Testimonials } from './Testimonials';
 import './home.scss';
