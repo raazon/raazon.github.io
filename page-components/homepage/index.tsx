f6da11f67e2c0ee93d313d2e60e82d1c33354a46
Razon Komar Pal <23665191+raazon@users.noreply.github.com>
2026-08-23 01:39:04 +0600

    Publish static site files for GitHub Pages hosting.

--- page-components/homepage/index.tsx
+++
@@ -1,7 +0,0 @@
-
-export { default as AboutMe } from './AboutMe';
-export { default as Overview } from './Overview';
-export { default as Posts } from './Posts';
-export { default as Projects } from './Projects';
-export { default as Testimonials } from './Testimonials';
-import './home.scss';
--- utils/letterLimit.ts
+++
@@ -1,8 +0,0 @@
-export function letterLimit(text: string, limit: number): string {
-	if (!text) return "";
-
-	const cleanText = text.trim();
-	return cleanText.length > limit
-		? cleanText.slice(0, limit) + "..."
-		: cleanText;
-}
